d6aaa146959810aeaad5546aa327e40b321eb357
olaitankareem01 <64306890+olaitankareem01@users.noreply.github.com>
2020-06-03 07:02:00 -0700

    Rahman.js
    added my javascript file for the second task

---
+++ scripts/Rahman.js
@@ -0,0 +1,7 @@
+function myinfo(fullname, id, language) {
+  console.log(
+    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  );
+}
+myinfo("kareem Abd-rahman", "HNG-04895", "Javascript");
+
